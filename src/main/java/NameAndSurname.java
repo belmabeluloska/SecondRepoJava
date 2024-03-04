03f740f4cb4a939a24e692844482299a70ddadb8
belmabeluloska <belmabeluloska@gmail.com>
2024-03-04 01:36:25 -0800

    Add NameAndSurname.java

---
+++ src/main/java/NameAndSurname.java
@@ -0,0 +1,11 @@
+
+public class NameAndSurname {
+public static void main (String[] args) {
+	
+	String name = "Belma";
+	String surname = "Beluloska";
+	
+	System.out.println(name + " " + surname);
+	
+}
+}
